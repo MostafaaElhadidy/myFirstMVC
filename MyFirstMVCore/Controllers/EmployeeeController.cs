ef9a4fc1d1ca50035fe3cd84f6cb5259795f9148
Mostafa Elhadidy <mostafafire2075@yahoo.com>
2024-09-21 00:46:57 +0300

    my first commit

---
+++ MyFirstMVCore/Controllers/EmployeeeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstMVCore.Controllers
+{
+    public class EmployeeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult showContent()
+        {
+            #region oldCode
+            //ContentResult result = new ContentResult();
+            //result.Content = "Hello from content result";
+            //return result;
+            #endregion
+            return Content("Hello from show contetntaaaa"); 
+        }
+
+        public ViewResult showView()
+        {
+            return View("MyView");
+        }
+
+        public ActionResult showMix()
+        {
+            var currentTime = TimeProvider.System.GetLocalNow();
+
+            if(currentTime.DayOfWeek == DayOfWeek.Thursday)
+            {
+                return View("MyView");
+            }
+            else
+            {
+                return Content("Today is not sunday bitch!!");
+            }
+        }
+        public IActionResult showMsg(string name, int age)
+        {
+            return Content($"Aloha There ! my name is {name} and my age is {age}");
+        }
+    }
+}
---
+++ MyFirstMVCore/Controllers/sasaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstMVCore.Controllers
+{
+    public class sasaController : Controller
+    {
+        public IActionResult myName()
+        {
+            return View();
+        }
+    }
+}
